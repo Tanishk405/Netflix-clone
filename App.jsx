ff2a2a3e562589ff4795dbba440ca4dbc4d91f4f
Tanishk405 <dtanish2580@gmail.com>
2025-07-19 22:28:14 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import Home from "./pages/Home/Home"
-import { Routes, Route, useNavigate } from 'react-router-dom'
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom'
 import LandingPage from "./pages/LandingPage/Landing"
 import Login from "./pages/Login/Login"
 import Player from "./pages/Player/Player"
@@ -8,63 +8,60 @@ import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from './firebase'
 import { Helmet, HelmetProvider } from 'react-helmet-async';  // Changed from react-helmet
 
+
+
+
+
+
+// ... existing imports ...
+
 function App() {
-
   const navigate = useNavigate();
+  const location = useLocation();
 
-
-  // useEffect(()=>{
-  //   onAuthStateChanged(auth, async (user)=>{
-  //     if(user){
-  //       console.log("Logged In");
-  //       navigate("/");
-  //     }else{
-  //       console.log("Logged Out");
-  //       navigate("/login");
-  //     }
-  //   })
-  // })
   useEffect(() => {
-  const unsubscribe = onAuthStateChanged(auth, (user) => {
-    const currentPath = window.location.pathname;
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      const currentPath = location.pathname;
 
-    if (user) {
-      // If logged in, redirect away from landing/login to home
-      if (currentPath === '/login' || currentPath === '/beforepage') {
-        navigate('/');
+      if (user) {
+        if (currentPath === '/login' || currentPath === '/beforepage') {
+          navigate('/');
+        }
+      } else {
+        if (currentPath === '/') {
+          navigate('/beforepage');
+        }
       }
-    } else {
-      // If not logged in and currently at `/`, go to landing
-      if (currentPath === '/') {
-        navigate('/beforepage');
-      }
-    }
-  });
+    });
 
-  return () => unsubscribe();
-}, [navigate]);
+    return () => unsubscribe();
+  }, [navigate, location.pathname]);
 
-
-
+  const handleSearch = (result) => {
+    navigate(`/player/movie/${result.id}`);
+  };
 
   return (
-      <HelmetProvider>
+    <HelmetProvider>
       <Helmet>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Helmet>
-      {/* Your app content */}
+      <div className='App'>
+        <Routes>
+          {/* <Route path='/' element={<Home onSearch={handleSearch} />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='beforepage' element={<LandingPage />} />
+          <Route path='/player/:id' element={<Player />} /> */}
 
-    <div className='App'>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='beforepage' element={<LandingPage/>} />
-        <Route path='/player/:id' element={<Player/>} />
-      </Routes>
+            <Route path='/' element={<Home onSearch={handleSearch} />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='beforepage' element={<LandingPage />} />
+            <Route path='/player/:type/:id' element={<Player />} />
 
-    </div>
+        </Routes>
+      </div>
     </HelmetProvider>
-  )
+  );
 }
 
-export default App
+export default App;
